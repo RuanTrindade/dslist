db5dc196a17c8dde8bda27274a041b7e91a0548f
Ruan Trindade <ruansilvapereira2005@gmail.com>
2025-08-19 15:29:50 -0300

    Game repository, service, controller

---
+++ src/main/java/com/ruantrindade/dslist/controllers/GameController.java
@@ -0,0 +1,27 @@
+package com.ruantrindade.dslist.controllers;
+
+
+import com.ruantrindade.dslist.dto.GameMinDTO;
+import com.ruantrindade.dslist.entities.Game;
+import com.ruantrindade.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+
+}
